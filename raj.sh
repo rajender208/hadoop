60952df7164150b45e047eea249ca3c9b9777db0
rajender208 <chennurirajender@gmail.com>
2017-09-01 00:29:45 +0530

    addin raj.sh

---
+++ raj.sh
@@ -0,0 +1,19 @@
+#!raj
+var1=Hello
+var2=raj
+
+#echo "$var1 $var"
+
+echo Enter your name:
+read name
+#echo this script name is $0
+#echo the 1st argumnt is $1
+#echo the 2nd arg is $2
+
+#echo the third argmnt is $3
+
+#echo \$ $$ represents PID of the script
+#echo \# $# represents total no of argmnts
+#echo \* $* reprsnts all argumts as single string
+
+#echo \? $? reprsnts the returned code
